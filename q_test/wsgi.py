0149bad469f8c472220f8892346a4f3863853fd9
nguyenleuyn0810 <nguyenthileuyen2005@gmail.com>
2026-04-26 17:36:55 +0700

    Fix DJANGO_SETTINGS_MODULE to q_test

--- q_test/wsgi.py
+++ q_test/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'q_test1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'q_test.settings')
 
 application = get_wsgi_application()
