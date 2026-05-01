331244ec69d663aa680811303d7cb37e856536a4
truclnb1405dn <truclnb1405.dn@gmail.com>
2026-05-02 00:05:13 +0700

    Update urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -22,17 +22,6 @@ urlpatterns = [
     path('nop-bai/<int:ma_ket_qua>/', views.nop_bai, name='nop_bai'),
     path('tra-cuu-ket-qua/', views.tra_cuu_ket_qua, name='tra_cuu_ket_qua'),
     path('xem-lai-bai-lam/<int:ma_ket_qua>/', views.xem_lai_bai_lam, name='xem_lai_bai_lam'),
-
-    # 4. Luồng của Người Dạy (Giảng viên)
-    path('quan-ly-de-thi/', views.quan_ly_de_thi, name='quan_ly_de_thi'),
-    path('tao-de-thi/', views.tao_de_thi_moi, name='tao_de_thi_moi'),
-    path('xoa-de/<int:ma_de_thi>/', views.xoa_de_thi, name='xoa_de_thi'),
-
-    path('ngan-hang-cau-hoi/', views.ngan_hang_cau_hoi, name='ngan_hang_cau_hoi'),
-    path('luu-ngan-hang-moi/', views.luu_ngan_hang_moi, name='luu_ngan_hang_moi'),
-
-    path('quan-ly-ket-qua/', views.ds_bai_thi_da_tao, name='ds_bai_thi_da_tao'),
-    path('ket-qua/<int:ma_de_thi>/', views.xem_ket_qua_chi_tiet, name='xem_ket_qua_chi_tiet'),
-    path('chi-tiet-bai-lam/<int:ma_ket_qua>/', views.chi_tiet_bai_lam, name='chi_tiet_bai_lam'),
+    path('api/luu-nhap/<int:ma_ket_qua>/', views.api_luu_nhap, name='api_luu_nhap'),
     path('api/luu-nhap/<int:ma_ket_qua>/', views.api_luu_nhap, name='api_luu_nhap'),
 ]
